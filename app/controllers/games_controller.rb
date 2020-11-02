20e8c0a5814525e4325abb936dce2b16c3a48ed2
Vasco Oliveira <vascooliveira@outlook.com>
2020-11-02 22:26:10 +0000

    challenge completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
